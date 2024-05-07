4af28ebb044f29526e0b7069ffae00c2ca1768f0
Dhairya jain <102275564+dhairyajainn@users.noreply.github.com>
2024-05-07 08:36:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+    //   console.log(item.content);
+    //   console.log(item.author);
+
+      quote.innerHTML = item.content;
+      author.innerHTML = item.author;
+    });
+};
+
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
